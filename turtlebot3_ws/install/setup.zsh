c5d09aabdbb013d0560dd6204d9deaab4c8422d8
Marc Castello Marti <marccastello1205@gmail.com>
2022-03-15 12:29:50 +0100

    Servidor mapes

--- turtlebot3_ws/install/setup.zsh
+++ turtlebot3_ws/install/setup.zsh
@@ -22,7 +22,10 @@ _colcon_prefix_chain_zsh_source_script() {
 COLCON_CURRENT_PREFIX="/opt/ros/foxy"
 _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/natxo/turtlebot3_ws/install"
+COLCON_CURRENT_PREFIX="/home/marc/install"
+_colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
+# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
+COLCON_CURRENT_PREFIX="/home/marc/turtlebot3_ws/install"
 _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 
 # source this prefix
