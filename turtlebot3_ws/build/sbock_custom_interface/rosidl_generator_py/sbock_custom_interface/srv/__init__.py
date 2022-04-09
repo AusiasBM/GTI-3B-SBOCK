ae1641782fff7ec6645470d1b4b975a37dcf2656
ausiasbm <ausiasbm@gmail.com>
2022-04-09 09:35:44 +0000

    cache de git borrada

--- turtlebot3_ws/build/sbock_custom_interface/rosidl_generator_py/sbock_custom_interface/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from sbock_custom_interface.srv._my_move_msg import MyMoveMsg  # noqa: F401
--- turtlebot3_ws/build/sbock_my_world/CMakeFiles/sbock_my_world_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sbock_my_world_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sbock_my_world_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
