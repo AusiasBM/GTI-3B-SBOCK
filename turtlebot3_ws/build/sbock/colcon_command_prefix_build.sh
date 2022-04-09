fd3a111176ade44e438490e6b5860f6408a91b44
ausiasbm <ausiasbm@gmail.com>
2022-04-09 09:21:07 +0000

    carpetas build log install borradas

--- turtlebot3_ws/build/sbock/colcon_command_prefix_build.sh
+++
@@ -1,8 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/pere/GTI-3B-SBOCK/turtlebot3_ws/install/provide_map/share/provide_map/package.sh"
-. "/home/pere/GTI-3B-SBOCK/turtlebot3_ws/install/sbock_custom_interface/share/sbock_custom_interface/package.sh"
-. "/home/pere/GTI-3B-SBOCK/turtlebot3_ws/install/sbock_my_world/share/sbock_my_world/package.sh"
-. "/home/pere/GTI-3B-SBOCK/turtlebot3_ws/install/sbock_nav_position/share/sbock_nav_position/package.sh"
-. "/home/pere/GTI-3B-SBOCK/turtlebot3_ws/install/sbock_nav_route/share/sbock_nav_route/package.sh"
-. "/home/pere/GTI-3B-SBOCK/turtlebot3_ws/install/sbock_position_publisher/share/sbock_position_publisher/package.sh"
-. "/home/pere/GTI-3B-SBOCK/turtlebot3_ws/install/sbock_robot_position_subscriber/share/sbock_robot_position_subscriber/package.sh"
