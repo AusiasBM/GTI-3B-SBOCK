2708cfe9a99cd2d60be28a65a0401b64590db76c
Aitor Benítez Estruch <aibes1001@gmail.com>
2022-05-17 23:23:41 +0200

    Implementat tractament digital de imatge amb CV2

--- turtlebot3_ws/galactic/src/sbock/sbock_capture_image/sbock_capture_image/capturar.py
+++ turtlebot3_ws/galactic/src/sbock/sbock_capture_image/sbock_capture_image/capturar.py
@@ -24,10 +24,43 @@ class Ros2OpenCVImageConverter(Node):
             print(e)
 
         cv2.imshow("Imagen capturada", cv_image)
-
+        
         cv2.imwrite('src/img/img.jpg', cv_image)
         cv2.waitKey(0) #aprieta una tecla 
         cv2.destroyAllWindows()
+        
+        #Convertim la imatge a escala de grisos
+        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+
+        #Apliquem filtre gaussià amb màscara de 11x11
+        blur = cv2.GaussianBlur(gray, (11, 11), 0)
+
+        #Apliquem umbralització de la imatge amb mètode de Otsu
+        ret, otsu = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        
+        #Operació d'erosió per a eliminar bordes eixints 
+        eroded = cv2.erode(otsu, (1, 1), iterations=30)
+
+        #Operació dilate per a rellenar huecos en els bordes
+        dilated = cv2.dilate(eroded, (1, 1), iterations=20)
+
+        #Apliquem detector de bordes de Canny
+        canny = cv2.Canny(dilated, 70, 100)
+        
+        #Comptabilitzem bordes per a determinar el nº de objectes
+        (cnt, hierarchy) = cv2.findContours(
+            canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        
+        #Dibuixem els bordes i mostrem la imatge a color captada amb els bordes detectats
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        cv2.drawContours(rgb, cnt, -1, (0, 255, 0), 2)
+
+        cv2.imshow("My Image",rgb)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+ 
+
+        self.get_logger().info("Objetos detectados : ", len(cnt))
 
 
 def main(args=None):
@@ -36,7 +69,8 @@ def main(args=None):
     img_converter_object = Ros2OpenCVImageConverter()    
        
     try:
-        rclpy.spin(img_converter_object)
+        #rclpy.spin(img_converter_object)
+        rclpy.spin_once(img_converter_object)
     except KeyboardInterrupt:
         img_converter_object.destroy_node()
         print("Fin del programa!")
