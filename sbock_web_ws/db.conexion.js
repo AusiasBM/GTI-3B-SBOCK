f58f4b44d9515261874ca73d4cc1aca685d57cab
ausiasbm <ausiasbm@gmail.com>
2022-06-06 13:55:39 +0000

    conexion con BBDD correcta

--- sbock_web_ws/db.conexion.js
+++ sbock_web_ws/db.conexion.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const conexionDB = async () => {
     try {
-        const DB = await mongoose.connect('mongodb://localhost:27017/sbock');
-            //'mongodb+srv://ausias:wpj8kbe.xra8FGA9hyq@cluster0.q37va.mongodb.net/sbock');
+        const DB = await mongoose.connect('mongodb+srv://ausias:wpj8kbe.xra8FGA9hyq@cluster0.q37va.mongodb.net/sbock');
         console.log("Conectado con Mongo, ", DB.connection.name);
     } catch (error) {
         console.log(error);
