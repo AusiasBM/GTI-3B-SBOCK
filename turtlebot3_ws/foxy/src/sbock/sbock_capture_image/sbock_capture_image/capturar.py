af9b6b2c22e3fc427bde21c6c1787532fa4eeaba
Aitor Benítez Estruch <aibes1001@gmail.com>
2022-06-08 08:41:18 +0200

    H8 -> T1 Paquet de predicció Yolo

--- turtlebot3_ws/foxy/src/sbock/sbock_capture_image/sbock_capture_image/capturar.py
+++ turtlebot3_ws/foxy/src/sbock/sbock_capture_image/sbock_capture_image/capturar.py
@@ -25,61 +25,7 @@ class Ros2OpenCVImageConverter(Node):
             print(e)
         
         cv2.imwrite('../../sbock_web_ws/public/app/imgs/img.jpg', cv_image)
-        cv2.imshow("Imagen capturada", cv_image)
-        cv2.waitKey(0) #aprieta una tecla 
-        cv2.destroyAllWindows()
         
-        #Convertim la imatge a escala de grisos
-        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("Imagen capturada", gray)
-        cv2.waitKey(0) #aprieta una tecla 
-        cv2.destroyAllWindows()
-
-        #Apliquem filtre gaussià amb màscara de 11x11
-        blur = cv2.GaussianBlur(gray, (11, 11), 0)
-        cv2.imshow("Imagen capturada", blur)
-        cv2.waitKey(0) #aprieta una tecla 
-        cv2.destroyAllWindows()
-
-        #Apliquem umbralització de la imatge amb mètode de Otsu
-        ret, otsu = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        cv2.imshow("Imagen capturada", otsu)
-        cv2.waitKey(0) #aprieta una tecla 
-        cv2.destroyAllWindows()
-        
-        #Operació d'erosió per a eliminar bordes eixints 
-        eroded = cv2.erode(otsu, (1, 1), iterations=30)
-        cv2.imshow("Imagen capturada", eroded)
-        cv2.waitKey(0) #aprieta una tecla 
-        cv2.destroyAllWindows()
-
-        #Operació dilate per a rellenar huecos en els bordes
-        dilated = cv2.dilate(eroded, (1, 1), iterations=20)
-        cv2.imshow("Imagen capturada", dilated)
-        cv2.waitKey(0) #aprieta una tecla 
-        cv2.destroyAllWindows()
-
-        #Apliquem detector de bordes de Canny
-        canny = cv2.Canny(dilated, 70, 100)
-        
-        cv2.imshow("Imagen capturada", canny)
-        cv2.waitKey(0) #aprieta una tecla 
-        cv2.destroyAllWindows()
-        
-        #Comptabilitzem bordes per a determinar el nº de objectes
-        (cnt, hierarchy) = cv2.findContours(
-            canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        
-        #Dibuixem els bordes i mostrem la imatge a color captada amb els bordes detectats
-        rgb = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-        cv2.drawContours(rgb, cnt, -1, (0, 255, 0), 2)
-
-        cv2.imshow("My Image",rgb)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
- 
-
-        self.get_logger().info("Objetos detectados: " + str(len(cnt)))
 
 
 def main(args=None):
